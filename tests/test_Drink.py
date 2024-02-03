4c15b4c918b9d28388e2e317475da324c181f2d7
dnguyen41 <davidnguyen6828@Yahoo.com>
2024-02-02 20:41:03 -0600

    Attempted fixtures

--- tests/test_Drink.py
+++ tests/test_Drink.py
@@ -1,39 +1,41 @@
 import pytest
 from Drink import Drink
 
-test = Drink('pokeacola', {'blueberry', 'lime'}, 3)
+@pytest.fixture
+def test_drink():
+    return Drink('pokeacola', {'blueberry', 'lime'}, 3)
 
 class TestDrink:
     # Retrieving flavors from drink
-    def test_get_flavors(self):
-        assert test.get_flavors() == {'blueberry', 'lime'}
+    def test_get_flavors(self, test_drink):
+        assert test_drink.get_flavors() == {'blueberry', 'lime'}
 
     # Retrieving base from drink
-    def test_get_base(self):
-        assert test.get_base() == 'pokeacola'
+    def test_get_base(self, test_drink):
+        assert test_drink.get_base() == 'pokeacola'
 
     # Retrieving total from drink
-    def test_get_total(self):
-        assert test.get_total() == 3
+    def test_get_total(self, test_drink):
+        assert test_drink.get_total() == 3
 
     # Retrieving total amount of flavors in drink
-    def test_get_num_flavors(self):
-        assert test.get_num_flavors() == 2
+    def test_get_num_flavors(self, test_drink):
+        assert test_drink.get_num_flavors() == 2
 
     # Setting flavors in drink
-    def test_set_flavors(self):
-        test.set_flavors({'lemon', 'lime'})
-        assert test.get_flavors() == {'lemon', 'lime'}
+    def test_set_flavors(self, test_drink):
+        test_drink.set_flavors({'lemon', 'lime'})
+        assert test_drink.get_flavors() == {'lemon', 'lime'}
 
         # Testing if invalid flavor entry triggers exception
         with pytest.raises(Exception):
-            test.set_flavors({'soda', 'lime'}) 
+            test_drink.set_flavors({'soda', 'lime'}) 
 
     # Adding flavor to drink
-    def test_add_flavor(self):
-        test.add_flavor('mint')
-        assert test.get_flavors() == {'lemon','lime','mint'}
+    def test_add_flavor(self, test_drink):
+        test_drink.add_flavor('mint')
+        assert test_drink.get_flavors() == {'blueberry','lime','mint'}
 
         #Testing if invalid flavor added triggers exception
         with pytest.raises(Exception):
-            test.add_flavor('blood')
+            test_drink.add_flavor('blood')
--- tests/test_Order.py
+++ tests/test_Order.py
@@ -2,35 +2,45 @@ import pytest
 from Order import Order
 from Drink import Drink
 
-drink_a = Drink('pokeacola', {'blueberry', 'lime'}, 3)
-drink_b = Drink('hill fog', {'cherry'}, 2)
-drink_c = Drink('leaf wine', {'strawberry', 'mint', 'lemon'}, 2)
+@pytest.fixture
+def drink_a():
+    return Drink('pokeacola', {'blueberry', 'lime'}, 3)
 
-test = Order([drink_a, drink_b])
+@pytest.fixture
+def drink_b():
+    return Drink('hill fog', {'cherry'}, 2)
+
+@pytest.fixture
+def drink_c():
+    return Drink('leaf wine', {'strawberry', 'mint', 'lemon'}, 2)
+
+@pytest.fixture
+def test_order():
+    return Order([drink_a, drink_b])
 
 class TestOrder:
     # Retrieving drinks from an order
-    def test_get_items(self):
-        assert test.get_items() == [drink_a, drink_b]
+    def test_get_items(self, test_order):
+        assert test_order.get_items() == [drink_a, drink_b]
                 
     # Retrieving the amount of items in an order
-    def test_num_items(self):
-        assert test.get_num_items() == 2
+    def test_num_items(self, test_order):
+        assert test_order.get_num_items() == 2
 
     # Calculate total for an order
-    def test_get_total(self):
-        assert test.get_total() == 5
+    def test_get_total(self, test_order):
+        assert test_order.get_total() == 5
 
     # Testing addition of a drink to an order
-    def test_add_item(self):
-        test.add_item(drink_c)
-        assert test.get_items() == [drink_a, drink_b, drink_c]
+    def test_add_item(self, test_order):
+        test_order.add_item(drink_c)
+        assert test_order.get_items() == [drink_a, drink_b, drink_c]
 
     # Testing removal of a drink from an order
-    def test_remove_item(self):
-        test.remove_item(1)
-        assert test.get_items() == [drink_a, drink_c]
+    def test_remove_item(self, test_order):
+        test_order.remove_item(1)
+        assert test_order.get_items() == [drink_a]
 
         #Testing if an invalid index returns an exception
         with pytest.raises(Exception):
-            test.remove_item(2)
+            test_order.remove_item(2)
